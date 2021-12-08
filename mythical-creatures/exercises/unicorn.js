f82cd9b4dc01bd53f300f951e064da19f38a21ab
Rory Magee <roryemagee@gmail.com>
2021-12-07 20:28:26 -0700

    Add unicorn.js and tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,17 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = "white") {
     this.name = name;
     this.color = color;
+  };
+
+  isWhite() {
+    return false;
+  };
+
+  says(statement) {
+    return `**;* ${statement} *;**`
   }
-}
+
+};
 
 module.exports = Unicorn;
