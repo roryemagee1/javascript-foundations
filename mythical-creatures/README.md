80296acba4726ac0ebfb3866e48df5e7be7f1e5c
Rory Magee <roryemagee@gmail.com>
2021-12-07 20:46:41 -0700

    Complete dragon.js and tests

--- mythical-creatures/README.md
+++ mythical-creatures/README.md
@@ -16,8 +16,8 @@ Run the command `npm test mythical-creatures/test/unicorn-test.js`. To run other
 
 ### Creature Order
 
-- [ ] `unicorn`
-- [ ] `dragon`
+- [X] `unicorn`
+- [X] `dragon`
 - [ ] `vampire`
 - [ ] `hobbit`
 - [ ] `pirate`
--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+var foodCount = 0;
 
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  };
+
+  greet() {
+    return `Hi, ${this.rider}!`
+  };
+
+  eat() {
+    foodCount = foodCount + 1;
+    if (foodCount === 3) {
+      this.hungry = false;
+    };
+  };
+
+};
+
+module.exports = Dragon;
